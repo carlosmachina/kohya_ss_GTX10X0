3b805e020cb7302818773f559dc4fc8718e1033d
Carlos Carvalho <carlos.lm.carvalho@gmail.com>
2023-02-12 20:18:58 -0300

    Fix xformers req + setup

--- setup.bat
+++ setup.bat
@@ -15,38 +15,45 @@ if not %ERRORLEVEL% equ 0 goto noGit
 echo Setting execution policy to unrestricted
 Call PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList 'Set-ExecutionPolicy Unrestricted -Force' -Verb RunAs}"
 cd /d "%~dp0"
-echo cloning khoya_ss
-git clone https://github.com/bmaltais/kohya_ss.git
-cd kohya_ss
 
+echo updating this repository
+git pull
+
+echo:
 echo creating venv for python
 python -m venv venv
 call venv\Scripts\activate
 
 rem Upgrade PIP
+echo updating pip
 python -m pip install --upgrade pip
 
-set cuVersion=117
+set cuVersion=cu117
 set torchVersion=1.13.1+%cuVersion%
 set torchVisionVersion=0.14.1+%cuVersion%
 
-choice /c Yn y /m "Do you want to run with updated torch(1.13)?"
+echo:
+choice /c Yn /m "Do you want to run with updated torch(1.13)?"
 if ERRORLEVEL 2 (
-    set cuVersion=116
-    set torchVersion=1.12.1+%cuVersion%
-    set torchVisionVersion=0.13.1+%cuVersion%
+   set cuVersion=cu116
+   set torchVersion=1.12.1+%cuVersion%
+   set torchVisionVersion=0.13.1+%cuVersion%
 )
 
+echo:
 echo installing dependancies, this may take a while
-echo installing torch (%torchVersion%)+cu(%cuVersion%)
-pip install torch==%torchVersion% torchvision==%torchVisionVersion% --extra-index-url "https://download.pytorch.org/whl/cu%cuVersion%" > nul
+echo installing torch %torchVersion%
+pip install torch==%torchVersion% torchvision==%torchVisionVersion% --extra-index-url "https://download.pytorch.org/whl/%cuVersion%" > nul
 
+echo:
 echo installing other dependancies
 pip install --use-pep517 --upgrade -r requirements.txt > nul
 
+echo:
 echo installing xformers
 pip install install -U -I --no-deps xformers==0.0.16rc425 > nul
 
+echo:
 echo moving required bitsandbytes files
 IF NOT exist venv\Lib\site-packages\bitsandbytes (mkdir venv\Lib\site-packages\bitsandbytes)
 If NOT exist venv\Lib\site-packages\bitsandbytes\cuda_setup (mkdir venv\Lib\site-packages\bitsandbytes\cuda_setup)
@@ -54,27 +61,18 @@ copy bitsandbytes_windows\*.dll venv\Lib\site-packages\bitsandbytes > nul
 copy bitsandbytes_windows\cextension.py venv\Lib\site-packages\bitsandbytes > nul
 copy bitsandbytes_windows\main.py venv\Lib\site-packages\bitsandbytes\cuda_setup > nul
 
-choice /C Yn /M "Do you want to install the optional cudnn1.8 for faster training on high end 30X0 and 40X0 cards?"
-if ERRORLEVEL 2 goto complete
-
-echo installing cudnn1.8 for faster training on 40X0 cards
-curl "https://b1.thefileditch.ch/mwxKTEtelILoIbMbruuM.zip" -o "cudnn.zip"
-Call Powershell Expand-Archive "cudnn.zip" -DestinationPath ".\\"
-del "cudnn.zip"
-python tools\cudann_1.8_install.py
-rmdir cudnn_windows /s /q
-goto complete
-
 :pascalFix
-choice /C Yn /M "Do you have a 10X0 card?"
-if ERRORLEVEL 2 goto complete
+echo:
+choice /C YN /M "Do you have a 10X0 card?"
+if ERRORLEVEL 2 goto accelerate
 
 echo installing 10X0 card fix
-move ..\kohya_ss_Nvidia10xx\installables\libbitsandbytes_cudaall.dll venv\Lib\site-packages\bitsandbytes > nul
-move ..\kohya_ss_Nvidia10xx\installables\main.py venv\Lib\site-packages\bitsandbytes\cuda_setup > nul
-goto complete
+copy .\installables\libbitsandbytes_cudaall.dll venv\Lib\site-packages\bitsandbytes > nul
+copy .\installables\main.py venv\Lib\site-packages\bitsandbytes\cuda_setup > nul
+goto accelerate
 
 :noAdmin
+echo:
 echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
 set params= %*
 echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
@@ -86,6 +84,7 @@ exit /B
 :pythonChecker
 reg query "hkcu\Software\Python\PythonCore\3.10" > nul 2>&1
 if not %ERRORLEVEL% equ 0 (
+	echo:
 	echo Can't find Python 3.10 in current user registry; checking local machine registry...
 	rem Now we check HKLM...
 	
@@ -97,22 +96,30 @@ if not %ERRORLEVEL% equ 0 (
 		exit /b
 	)
 ) else (
+	echo:
 	echo Found Python version 3.10
 	rem return control to the caller
 	exit /b
 )
 
 :noPython
+echo:
 echo Can't continue as you do not have python 3.10 installed, please install python 3.10 and ensure you select the 'add to path' option. Then run this script again.
 goto end
 
 :noGit
+echo:
 echo Can't continue as you do not have git installed, please install it and ensure it's added to the path. Then run this script again.
 goto end
 
-:complete
+:accelerate
+echo:
 echo running accelerate config
-accelerate config > nul
+call accelerate config
+goto complete
+
+:complete
+echo:
 echo installation complete, to run the program run gui.bat
 
 :end
